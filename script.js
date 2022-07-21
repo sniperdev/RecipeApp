66fd667ddcb69e8cf77e3ae7b03c9da164ad9072
sniperdev <62942349+sniperdev@users.noreply.github.com>
2022-07-21 20:38:03 +0200

    add to favourites function

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getMealById(i) {
   const data = await response.json();
   return await data;
 }
-
+//TODO: querySelector wykonaj tylko raz po co za każdym razem wykonywane jest wyszukiwanie elementu
 async function showMeals() {
   for (let i = 0; i < 4; i++) {
     let meal = await getMealById(i);
@@ -23,6 +23,93 @@ async function showMeals() {
       <div class="info"><h1>${readyMeals[i].strMeal}</h1><i class="fa-solid fa-stopwatch"></i>5 min<i class="fa-solid fa-bookmark"></i>
       </div></div>`;
   }
+  findPopular();
+}
+showMeals();
+
+async function searchMeals(searchField) {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchField
+  );
+  const data = await response.json();
+  return await data;
+}
+//TODO: Połącz w jedną funkcje uniwersalną showSearchedMeals oraz showMeals, na pewno da się to zrobić uniwersalne
+function showSearchedMeals(searchedMeals) {
+  const searchElement = document.querySelector(".populars");
+  searchElement.innerHTML = `<h1>Search results</h1>`;
+  for (let i = 0; i < searchedMeals.meals.length; i++) {
+    searchElement.innerHTML += `<div class="popular"><img src="${searchedMeals.meals[i].strMealThumb}"
+           alt="">
+      <div class="info"><h1>${searchedMeals.meals[i].strMeal}</h1><i class="fa-solid fa-stopwatch"></i>5 min<i class="fa-solid fa-bookmark"></i>
+      </div></div>`;
+  }
+}
+async function search() {
+  let searchValue = document.querySelector(".searchField input").value;
+  let searchedMeals = await searchMeals(searchValue);
+  if (searchValue) showSearchedMeals(searchedMeals);
+  else {
+    document.querySelector(".populars").innerHTML = `<h1>Popular</h1>`;
+    await showMeals();
+  }
+  findPopular();
+}
+document.querySelector(".searchField button").addEventListener("click", search);
+document.querySelector(".searchField").addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    search();
+  }
+});
+function findPopular() {
+  let favs = document.querySelectorAll(".popular");
+  favs.forEach((element) => {
+    let bookmark = element.querySelector(".fa-bookmark");
+
+    bookmark.addEventListener("click", () => {
+      if (!bookmark.getAttribute("style")) {
+        bookmark.style.color = "red";
+        document.querySelector(".favs").innerHTML += `<div class="fav">
+      <img src="${element.querySelector("img").src}"
+           alt="${element.querySelector("h1").textContent}"><h1>${
+          element.querySelector("h1").textContent
+        }</h1><i class="fa-solid fa-x"></i>
+      <h1></h1>
+    </div>`;
+        findFromFav();
+      } else {
+        removeFromPopular(element);
+      }
+    });
+  });
+}
+function removeFromPopular(element) {
+  let favs = document.querySelectorAll(".fav");
+  element.querySelector(".fa-bookmark").removeAttribute("style");
+  favs.forEach((fav) => {
+    if (
+      fav.querySelector("h1").textContent ===
+      element.querySelector("h1").textContent
+    ) {
+      fav.remove();
+    }
+  });
 }
 
-showMeals();
+function findFromFav() {
+  let favs = document.querySelectorAll(".fav");
+  favs.forEach((element) => {
+    element.querySelector("i").addEventListener("click", () => {
+      document.querySelectorAll(".popular").forEach((el) => {
+        if (
+          element.querySelector("h1").textContent ===
+          el.querySelector("h1").textContent
+        ) {
+          el.querySelector(".fa-bookmark").removeAttribute("style");
+        }
+      });
+      element.remove();
+    });
+  });
+}
+findFromFav();
